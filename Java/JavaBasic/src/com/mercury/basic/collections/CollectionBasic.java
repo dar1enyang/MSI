c3b35b2cb9a93c6ba1da0a90dff0ae177bba73a7
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-08-09 17:33:36 -0400

    0809

--- Java/JavaBasic/src/com/mercury/basic/collections/CollectionBasic.java
+++ Java/JavaBasic/src/com/mercury/basic/collections/CollectionBasic.java
@@ -1,7 +1,9 @@
 package com.mercury.basic.collections;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.mercury.beans.Dog;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @program: JavaBasic
@@ -14,8 +16,24 @@ public class CollectionBasic {
     public static void main(String[] args) {
         // Generic
         List<Integer> l = new ArrayList<>();
+        List<Integer> l2 = new LinkedList<>();
 
         // Unboxing: covert its wrapper class object to primitive
         // Java include auto-boxing and auto-unboxing
+
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> synchronizedMap = Collections.synchronizedMap(map);
+
+        Map<Integer, Integer> ht = new Hashtable<>();
+        Map<Integer, Integer> chm = new ConcurrentHashMap<>();
+
+
+        chm.put(1, 1);
+        // Object needs to implement comparable
+
+        Map<Integer, String> tm = new TreeMap<>();
+
+        // Comparable
+
     }
 }
