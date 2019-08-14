87d2cea854dbfc244c89602cdfca355b94c192dd
Po-Yu Yang <corupted5354.cv99@g2.nctu.edu.tw>
2019-08-14 10:33:06 -0400

    0814 JDBC

---
+++ J2EE/jdbcDemo/src/main/java/com/mercury/jdbcDemo/App.java
@@ -0,0 +1,13 @@
+package com.mercury.jdbcDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Java/JavaBasic/src/com/mercury/practice/OverloadTest.java
@@ -0,0 +1,32 @@
+package com.mercury.practice;
+
+/**
+ * @program: JavaBasic
+ * @description: !!
+ * @author: yangdar1en
+ * @create: 2019-08-14 09:53
+ **/
+
+
+class Par  {
+    Par() {
+
+    }
+
+    public void method1(int s) {
+
+    }
+}
+
+class Sub extends Par {
+    Sub() {
+
+    }
+
+
+}
+public class OverloadTest {
+    public static void main(String[] args) {
+
+    }
+}
